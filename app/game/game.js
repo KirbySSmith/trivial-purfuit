b4c6b5253bbb4f810c4a2c60251bb62cfa84f885
Kirby Smith <ksmit207@jhu.edu>
2015-05-02 21:30:48 -0400

    #9: Game piece UI

--- app/game/game.js
+++ app/game/game.js
@@ -13,22 +13,17 @@
 
         vm.boardSpaces = [];
         vm.players = [];
-        vm.players.push(new Player(1, "Kirby"));
-        vm.players.push(new Player(2, "Tommy"));
-        vm.players.push(new Player(3, "Frances"));
-        vm.players.push(new Player(4, "Sam"));
+        vm.players.push(new Player(1, "Kirby", "#99cc99"));
+        vm.players.push(new Player(2, "Tommy", "#cc99cc"));
+        vm.players.push(new Player(3, "Frances", "#ffdb99"));
+        vm.players.push(new Player(4, "Sam", "#ffff7f"));
         vm.currentPlayerIndex = 0;
-
         D6.baseUrl = "assets/images/diceRoll/";
-        D6.dice(2, function(result){
-            vm.players[vm.currentPlayerIndex].updateLocation(result, promptForDirection);
-            if(vm.currentPlayerIndex < vm.players.length){
-                vm.currentPlayerIndex = vm.currentPlayerIndex + 1 < vm.players.length ? vm.currentPlayerIndex + 1 : 0 ;
-            }else{
 
-            }
-            console.log(result)
-        });
+
+
+
+        D6.dice(2, takeTurn);
         activate();
 
         function activate() {
@@ -37,10 +32,18 @@
                 vm.players.forEach(function(player){
                     player.boardLocation  = vm.boardSpaces[26];
                 });
+                vm.players[vm.currentPlayerIndex].currentPlayer= true;
             });
         }
 
-
+        function takeTurn(result) {
+            vm.players[vm.currentPlayerIndex].updateLocation(result, promptForDirection, nextTurn);
+        }
+        var nextTurn = function (){
+            vm.players[vm.currentPlayerIndex].currentPlayer= false;
+            vm.currentPlayerIndex = vm.currentPlayerIndex + 1 < vm.players.length ? vm.currentPlayerIndex + 1 : 0 ;
+            vm.players[vm.currentPlayerIndex].currentPlayer= true;
+        };
         var promptForDirection = function(directionList) {
             //Todo: a decision is need from user on which direction to travel
             return directionList[0];
