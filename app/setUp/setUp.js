127251e3781d76a00bcd1c8a0deea61b3e2ec2ed
Frances Advincula <fmadvincula@gmail.com>
2015-05-07 12:49:55 -0700

    UI for chooseing # of players and changing usernames

--- app/setUp/setUp.js
+++ app/setUp/setUp.js
@@ -9,5 +9,11 @@
 
     function SetUp(){
         var vm = this;
+        vm.playerCount = function (players) {
+            vm.count=[];
+            for (var i = 0; i < players; i++) {
+                vm.count.push({'count': i+1});
+            }
+        }
     }
 })();
